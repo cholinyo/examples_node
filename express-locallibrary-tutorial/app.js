43e05bc2acff008819e54e8ec54044e5dc775753
Vicente Caruncho Ramos <cholinyo@gmail.com>
2019-02-13 19:48:58 +0100

    adding routes

--- express-locallibrary-tutorial/app.js
+++ express-locallibrary-tutorial/app.js
@@ -7,20 +7,22 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
-var app = express();
+
+var app = express(); //Creamos el servidor
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
+app.set('views', path.join(__dirname, 'views'));  //Ruta de las templates
+app.set('view engine', 'pug'); // Motor para las templates
 
-app.use(logger('dev'));
+app.use(logger('dev'));  //declaramos los Middlewares
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
+app.use('/', indexRouter);  //app
 app.use('/users', usersRouter);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- express-locallibrary-tutorial/routes/users.js
+++ express-locallibrary-tutorial/routes/users.js
@@ -5,5 +5,7 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
-
+router.get('/users/', function(req, res, next) {
+  res.send('You are soo cool');
+});
 module.exports = router;
